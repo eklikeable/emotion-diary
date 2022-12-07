c539bafa088f4465125f290a0237148f6149bd0e
ekkim <eklikeable@kakao.com>
2022-12-07 20:19:04 +0900

    feat: DiaryList - header, sort, filter

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,58 @@
+import { useState, useContext, useEffect } from 'react';
+import { DiaryStateContext } from '../App';
+
+import MyHeader from './../components/MyHeader';
+import MyButton from './../components/MyButton';
+import DiaryList from './../components/DiaryList';
+
 const Home = () => {
+  const diaryList = useContext(DiaryStateContext);
+
+  const [data, setData] = useState([]);
+  const [curDate, setCurDate] = useState(new Date());
+  const headText = `${curDate.getFullYear()}년 ${curDate.getMonth() + 1}월`;
+
+  useEffect(() => {
+    //작성된 일기가 있는 경우에만
+    if (diaryList.length >= 1) {
+      // 월의 1일 계산
+      const firstDay = new Date(
+        curDate.getFullYear(),
+        curDate.getMonth(),
+        1
+      ).getTime();
+      // 월의 마지막날 계산
+      const lastDay = new Date(
+        curDate.getFullYear(),
+        curDate.getMonth() + 1,
+        0
+      ).getTime();
+
+      setData(
+        // 매 달 1일과 마지막날의 사이에 있는 날짜의 일기만 보여줌
+        diaryList.filter((it) => firstDay <= it.date && it.date <= lastDay)
+      );
+    }
+  }, [diaryList, curDate]);
+
+  const increaseMonth = () => {
+    setCurDate(
+      new Date(curDate.getFullYear(), curDate.getMonth() + 1, curDate.getDate())
+    );
+  };
+  const decreaseMonth = () => {
+    setCurDate(
+      new Date(curDate.getFullYear(), curDate.getMonth() - 1, curDate.getDate())
+    );
+  };
   return (
     <div>
-      <h1>Home</h1>
-      <p>이곳은 홈 입니다.</p>
+      <MyHeader
+        headText={headText}
+        leftChild={<MyButton text={'<'} onClick={decreaseMonth} />}
+        rightChild={<MyButton text={'>'} onClick={increaseMonth} />}
+      />
+      <DiaryList diaryList={data} />
     </div>
   );
 };
